85f6813cd968d8a8985aed72589e14e69d64604d
seanreed1111 <seanreed1111@gmail.com>
2016-10-24 19:01:34 -0400

    restructures files, adds some more css

---
+++ setup.js
@@ -0,0 +1,12 @@
+  var QUAKE_URL = 'http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojsonp';
+  function loadJSONp(url){
+    var script = document.createElement('script');
+    script.src = url;
+
+    var head = document.getElementsByTagName('head')[0];
+    head.appendChild(script);
+  }
+
+  var map = L.map('map').setView([33.858631, -118.279602], 7);
+  L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png').addTo(map)
+  
