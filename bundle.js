c78436b2fa2fc7a56b15f0086e48eb053e043318
seanreed1111 <seanreed1111@gmail.com>
2016-10-25 04:21:10 -0400

    does jsonp request using rx-DOM

--- bundle.js
+++ bundle.js
@@ -1,19 +1,18 @@
-var quakes$ = Rx.Observable.create(function(observer) {
-  window.eqfeed_callback = function(response) {
-    observer.onNext(response);
-    observer.onCompleted();
-  };
-
-  loadJSONP(QUAKE_URL);
+var quakes$ = Rx.DOM.jsonpRequest({
+  url: QUAKE_URL,
+  jsonpCallback: 'eqfeed_callback'
 })
-.do(function(x) {console.log(x)})
 .flatMap(function(dataset){
-  return Rx.Observable.from(dataset.features);
+  return Rx.Observable.from(dataset.response.features);
+})
+.map(function(quake){
+  return {
+    lat: quake.geometry.coordinates[1],
+    lng: quake.geometry.coordinates[0],
+    size: quake.properties.mag *10000
+  };
 });
 
 quakes$.subscribe(function(quake) {
-  var coords = quake.geometry.coordinates;
-  var size = quake.properties.mag * 10000;
-
-  L.circle([coords[1], coords[0]], size).addTo(map);
+  L.circle([quake.lat, quake.lng], quake.size).addTo(map);
 });
