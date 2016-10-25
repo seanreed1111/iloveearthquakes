229e24204a417c1fd5718445b81d51b51ebb4dd1
seanreed1111 <seanreed1111@gmail.com>
2016-10-25 05:19:53 -0400

    changes map scale, adds in TTD

--- bundle.js
+++ bundle.js
@@ -1,18 +1,49 @@
-var quakes$ = Rx.DOM.jsonpRequest({
-  url: QUAKE_URL,
-  jsonpCallback: 'eqfeed_callback'
-})
-.flatMap(function(obj){
-  return Rx.Observable.from(obj.response.features);
-})
-.map(function(quake){
-  return {
-    lat: quake.geometry.coordinates[1],
-    lng: quake.geometry.coordinates[0],
-    size: quake.properties.mag *10000
-  };
+
+
+// do 20 second long polls of data from USGS site, add new quakes as appropriate.
+
+var quakes$ = Rx.Observable.interval(20000).startWith(0).take(100)
+  .do(function(i) {console.log(i)})
+  .flatMap(function() {
+    return Rx.DOM.jsonpRequest({
+      url: QUAKE_URL,
+      jsonpCallback: 'eqfeed_callback', 
+    }).retry(5);
+  })
+  .flatMap(function(obj){
+    return Rx.Observable.from(obj.response.features);
+  })
+  .distinct(function(quake) {return quake.properties.code})
+  .map(function(quake){
+    return {
+      lat: quake.geometry.coordinates[1],
+      lng: quake.geometry.coordinates[0],
+      magnitude: quake.properties.mag,
+      time: quake.properties.time,
+      place: quake.properties.place
+    };
+  })
+  .do(function(x) {console.log(x)});
+
+// for leaflet map
+quakes$.subscribe(function(quake) {
+  L.circle([quake.lat, quake.lng], quake.magnitude*10000).addTo(map);
 });
 
-quakes$.subscribe(function(quake) {
-  L.circle([quake.lat, quake.lng], quake.size).addTo(map);
-});
+
+
+// TTD 
+// **show the initial feed of earthquake one at a time in timestamp order
+// ** build a dashboard that shows the place/magnitude of new quakes 
+// **make a game out of this? 
+// Game: Earthquake Bingo? 
+
+// I have no idea what Earthquake Bingo is yet but I want to play it!
+// use lat, long and/or magnitudes to generate bingo cards!
+// then pull time series and play the game...
+
+
+// Note: the distinct could potentially miss quakes that for example had incorrect data at first
+// if USGS sends through corrected data, it will be an quake 
+// with a code already seen before, not a distinct one.
+// is it work
--- setup.js
+++ setup.js
@@ -7,6 +7,6 @@
     head.appendChild(script);
   }
 
-  var map = L.map('map').setView([33.858631, -118.279602], 7);
+  var map = L.map('map').setView([33.858631, -118.279602], 3);
   L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png').addTo(map)
   
